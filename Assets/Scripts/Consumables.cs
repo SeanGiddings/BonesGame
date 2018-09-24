0e216b0b42d9249eddb00946c0c1487df186025b
SeanGiddings <seandgiddings@gmail.com>
2018-09-24 01:04:23 -0400

    Initial Commit, Completed 2D Roguelike Scavengers tutorial, playable

---
+++ Assets/Scripts/Consumables.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Consumables {
+
+    public static int pointsPerFood = 10;
+    public static int pointsPerSoda = 20;
+
+
+}
